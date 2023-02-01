e455a24dd4cdbc20dfb4ffd6df274d28a2c3b5cb
ddraigness <110378886+ddraigness@users.noreply.github.com>
2023-02-01 23:05:15 +0700

    belajar goroutines

---
+++ waitgroup_test.go
@@ -0,0 +1,50 @@
+package belajargolanggoroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+// WaitGroup
+func RunAsynchronous(group *sync.WaitGroup) {
+	defer group.Done()
+
+	group.Add(1)
+
+	fmt.Println("Hello")
+	time.Sleep(1 * time.Second)
+}
+
+func TestWaitGroup(t *testing.T) {
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go RunAsynchronous(group)
+	}
+	group.Wait()
+	fmt.Println("Complete")
+}
+
+// Once
+var counter = 0
+
+func OnlyOnce() {
+	counter++
+}
+
+func TestOnce(t *testing.T) {
+	once := sync.Once{}
+	group := sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		go func() {
+			once.Do(OnlyOnce)
+			group.Done()
+		}()
+	}
+	group.Wait()
+	fmt.Println("Counter", counter)
+}
